449ab011b681cd0590178bc5c78ac975f14d5723
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-22 18:59:56 +0100

    added getComments query to codebase

--- convex/comments.ts
+++ convex/comments.ts
@@ -21,3 +21,15 @@ export const addComment = mutation({
     })
   }
 })
+
+
+// get all comments for an interview
+export const getComments = query({
+  args: { interviewId: v.id("interviews") },
+  handler: async (ctx, args) => {
+    const comments = await ctx.db.query("comments")
+      .withIndex("by_interview_id", (q) => q.eq("interviewId", args.interviewId))
+      .collect();
+    return comments;
+  }
+})
